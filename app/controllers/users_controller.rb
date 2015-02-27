86ea7a0fe5c20d428057f6beabcecedf79ffe223
Gerardo Gonzalez <gonzalezrgerardo@gmail.com>
2015-02-26 21:19:21 -0800

    Finished user model and signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
-  end
+  	def new
+		@user = User.new
+  	end
+	
+	def show
+		@user = User.find(params[:id])
+	end
+	
+	def create
+   	@user = User.new(user_params)    # Not the final implementation!
+    	if @user.save
+      	# if successful submission is received do the following:
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+    	else 
+			# if there are errors with the form do the following:
+      	render 'new'
+    	end
+  	end
+	
+	private
+
+   	def user_params
+      	params.require(:user).permit(:name, :email, :password, :password_confirmation) # <- only parameters allowed
+    	end
 end
